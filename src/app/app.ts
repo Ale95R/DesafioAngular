b1420f73fc5f4a97451235a8dd0ac113dfa87986
Mike9426 <eddvall26@gmail.com>
2025-06-08 10:59:00 -0600

    app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,20 +3,32 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Navbar } from './navbar/navbar';
 import { Catalogo } from './catalogo/catalogo.component';
+import { CarritoComponent } from './carrito/carrito.component';
 import { Carrito } from './servicios/carrito';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
   styleUrls: ['./app.css'],
-  imports: [CommonModule, FormsModule, Navbar, Catalogo],
+  imports: [
+    CommonModule,
+    FormsModule,
+    Navbar,
+    Catalogo,
+    CarritoComponent,
+    RouterModule,
+  ],
 })
 export class App {
   mostrarCarrito = false;
+
   constructor(public carrito: Carrito) {}
 
-  @HostListener('document:toggleCart') toggleCart() {
+  @HostListener('document:toggleCart')
+  toggleCart() {
     this.mostrarCarrito = !this.mostrarCarrito;
   }
 
