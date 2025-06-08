a5b7c0593e7651f01c66adad480d3b0d436921d3
Mike9426 <eddvall26@gmail.com>
2025-06-08 10:58:07 -0600

    app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CarritoComponent } from './carrito/carrito.component';
+import { Catalogo } from './catalogo/catalogo.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Catalogo },
+  { path: 'carrito', component: CarritoComponent }
+];
